2c90c7256d5c41069e064b4c39b5ccc4c4738dd2
Alexander Kalik <alexanderk1996@gmail.com>
2017-11-02 14:41:23 -0400

    start sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
+
+  resource :users
   resources :estimates
   resources :projects
+  resource :sessions
 
-  root 'projects#index'
+  root 'sessions#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
